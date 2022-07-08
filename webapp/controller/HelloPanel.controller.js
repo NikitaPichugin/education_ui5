d959e727ba6a4ae300275fc262d8efef029ba325
Nikita Pichugin <nikita.pichugin@ajasp.ru>
2022-07-08 12:16:29 +0200

    Таблица json

--- webapp/controller/HelloPanel.controller.js
+++ webapp/controller/HelloPanel.controller.js
@@ -13,6 +13,22 @@ sap.ui.define(
         // show message
         MessageToast.show(sMsg)
       },
+
+      onOpenDialog: function () {
+        // create dialog lazily
+        if (!this.pDialog) {
+          this.pDialog = this.loadFragment({
+            name: 'sap.ui.demo.walkthrough.view.HelloDialog',
+          })
+          this.pDialog.then(function (oDialog) {
+            oDialog.open()
+          })
+        }
+      },
+
+      onCloseDialog: function () {
+        this.byId('helloDialog').close()
+      },
     })
   }
 )
